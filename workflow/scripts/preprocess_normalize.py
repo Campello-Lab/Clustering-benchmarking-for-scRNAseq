9bcf8b164e276d43e51d9e687f0f114c8673eba0
AlSzmigiel <aleksandraszmigiel58@gmail.com>
2026-04-29 17:11:20 +0200

    update

--- workflow/scripts/preprocess_normalize.py
+++ workflow/scripts/preprocess_normalize.py
@@ -4,7 +4,7 @@ import scipy.sparse as spa
 import matplotlib.pyplot as plt
 import os
 
-def preprocess_dataset(adata, input=snakemake.params.input_rep, batch_label =snakemake.params.batch_key, cell_label = snakemake.params.cell_label ):
+def preprocess_dataset(adata, input=snakemake.params.input_rep,  cell_label = snakemake.params.cell_label ):
     del adata.obsp
     del adata.obsm
     del adata.varm
@@ -38,43 +38,9 @@ def preprocess_dataset(adata, input=snakemake.params.input_rep, batch_label =sna
         #adata_p.X=adata_p.X.toarray()
         adata_p.layers["counts"] =adata_p.X.toarray()
         adata_p.X = adata_p.layers["counts"]
-    if batch_label is not None:
-        adata_p = remove_visualize_batch(adata_p, batch_label, cell_label)
     return adata_p
 
-def remove_visualize_batch(adata, batch_key, cell_type_key):
-    # Perform PCA before batch correction on the copy - we don't need to write this to the object
-    # as this is before batch correction and only for visualization purpose.
-    adata_copy = adata.copy()
-    sc.pp.pca(adata_copy)
-    sc.pp.neighbors(adata_copy, use_rep="X_pca")
-    sc.tl.umap(adata_copy)
-    # Create a figure with subplots
-    fig, axes = plt.subplots(2, 2, figsize=(12, 10))    
-    # UMAP before batch correction, colored by batch
-    sc.pl.umap(adata_copy, color=batch_key, ax=axes[0, 0], show=False, title="Raw - Batch")    
-    # UMAP before batch correction, colored by cell type
-    sc.pl.umap(adata_copy, color=cell_type_key, ax=axes[0, 1], show=False, title="Raw - Cell Type")
-    # Apply PCA on dataset
-    sc.pp.pca(adata)
-    # Apply Harmony batch correction
-    sc.external.pp.harmony_integrate(adata, batch_key)    
-    # UMAP after batch correction
-    sc.pp.neighbors(adata, use_rep="X_pca_harmony", key_added = 'correction')
-    sc.tl.umap(adata, neighbors_key='correction')    
-    # UMAP after batch correction, colored by batch
-    sc.pl.umap(adata, color=batch_key, ax=axes[1, 0], show=False, title="Harmony Corrected - Batch")    
-    # UMAP after batch correction, colored by cell type
-    sc.pl.umap(adata, color=cell_type_key, ax=axes[1, 1], show=False, title="Harmony Corrected - Cell Type")    
-    # Adjust layout and save the combined figure
-    output_path = snakemake.params.visualization
-    directory = os.path.dirname(output_path)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    plt.tight_layout()
-    plt.savefig(snakemake.params.visualization)
-    plt.close()
-    return adata
+
 
 
 processed = preprocess_dataset(sc.read(str(snakemake.input)))
